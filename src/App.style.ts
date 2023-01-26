4d88b8764bb8e2c2824137e6ddaba097dc6a791c
joaorezend3 <joaovitorezende45@gmail.com>
2023-01-26 10:30:22 -0300

    first commit

---
+++ src/App.style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+export const container =styled.div`
+background-color: #17181F;
+color: #797A81;
+`; 
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as C from './App.style'
+const App = () => {
+  return(
+    <C.container>
+      ...
+    </C.container>
+
+  )
+  };
+export default App;
